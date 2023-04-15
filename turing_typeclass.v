4b38d623da96b4ac13c6e4f79f77dd768891e05e
Thalia Archibald <thalia@archibald.dev>
2023-04-15 16:42:49 -0600

    Use intermediate tape variables
    This makes every constraint unique and allows for non-termination.

--- turing_typeclass.v
+++ turing_typeclass.v
@@ -5,10 +5,10 @@ Local Notation "1" := true.
 
 (** A Smallfuck program *)
 Inductive prog : Type :=
-  | PRight (next : prog)
-  | PLeft (next : prog)
-  | PFlip (next : prog)
-  | PLoop (body : prog) (next : prog)
+  | PRight (next : prog) (* > move the tape right *)
+  | PLeft (next : prog) (* < move the tape left *)
+  | PFlip (next : prog) (* * flip the current cell *)
+  | PLoop (body : prog) (next : prog) (* [ … ] repeat while the cell is 1 *)
   | PEnd.
 
 (** An infinite tape, where [cell] is the current cell and [ltape] and [rtape]
@@ -19,58 +19,90 @@ Record tape : Type := Tape {
   rtape : list bool;
 }.
 
-Class Exec (p : prog) (t : tape) : Type := {
-  exec : tape;
+Class Exec (p : prog) (t t' : tape) : Type := {
+  exec := t';
 }.
 
-#[export] Instance Exec_Right_cons p c lt rc rt
-    `{E : Exec p (Tape rc (c :: lt) rt)} :
-    Exec (PRight p) (Tape c lt (rc :: rt)) := {
-  exec := E.(exec);
-}.
-#[export] Instance Exec_Right_nil p c lt
-    `{E : Exec p (Tape 0 (c :: lt) [])} :
-    Exec (PRight p) (Tape c lt []) := {
-  exec := E.(exec);
-}.
+#[export] Instance Exec_Right_cons p c lt rc rt t'
+  `{Exec p (Tape rc (c :: lt) rt) t'} :
+  Exec (PRight p) (Tape c lt (rc :: rt)) t' := {}.
+#[export] Instance Exec_Right_nil p c lt t'
+  `{Exec p (Tape 0 (c :: lt) []) t'} :
+  Exec (PRight p) (Tape c lt []) t' := {}.
 
-#[export] Instance Exec_Left_cons p c lc lt rt
-    `{E : Exec p (Tape lc lt (c :: rt))} :
-    Exec (PLeft p) (Tape c (lc :: lt) rt) := {
-  exec := E.(exec);
-}.
-#[export] Instance Exec_Left_nil p c rt
-    `{E : Exec p (Tape 0 [] (c :: rt))} :
-    Exec (PLeft p) (Tape c [] rt) := {
-  exec := E.(exec);
-}.
+#[export] Instance Exec_Left_cons p c lc lt rt t'
+  `{Exec p (Tape lc lt (c :: rt)) t'} :
+  Exec (PLeft p) (Tape c (lc :: lt) rt) t' := {}.
+#[export] Instance Exec_Left_nil p c rt t'
+  `{Exec p (Tape 0 [] (c :: rt)) t'} :
+  Exec (PLeft p) (Tape c [] rt) t' := {}.
 
-#[export] Instance Exec_Flip p c lt rt
-    `{E : Exec p (Tape (negb c) lt rt)} :
-    Exec (PFlip p) (Tape c lt rt) := {
-  exec := E.(exec);
-}.
+#[export] Instance Exec_Flip p c lt rt t'
+  `{Exec p (Tape (negb c) lt rt) t'} :
+  Exec (PFlip p) (Tape c lt rt) t' := {}.
 
-#[export] Instance Exec_Loop_0 b p lt rt
-    `{E : Exec p (Tape 0 lt rt)} :
-    Exec (PLoop b p) (Tape 0 lt rt) := {
-  exec := E.(exec);
-}.
-#[export] Instance Exec_Loop_1 b p lt rt
-    `{Eb : Exec b (Tape 1 lt rt)}
-    `{Ep : Exec (PLoop b p) Eb.(exec)} :
-    Exec (PLoop b p) (Tape 1 lt rt) := {
-  exec := Ep.(exec);
-}.
+#[export] Instance Exec_Loop_0 b p lt rt t'
+  `{Exec p (Tape 0 lt rt) t'} :
+  Exec (PLoop b p) (Tape 0 lt rt) t' := {}.
+#[export] Instance Exec_Loop_1 b p lt rt t' t''
+  `{Exec b (Tape 1 lt rt) t'}
+  `{Exec (PLoop b p) t' t''} :
+  Exec (PLoop b p) (Tape 1 lt rt) t'' := {}.
 
-#[export] Instance Exec_End t : Exec PEnd t := {
-  exec := t;
-}.
+#[export] Instance Exec_End t :
+  Exec PEnd t t := {}.
 
 Set Typeclasses Debug.
-Definition exec_end `{E : Exec PEnd (Tape 0 [] [])} := E.(exec).
-Check exec_end.
-Definition exec_right `{E : Exec (PRight PEnd) (Tape 0 [] [])} := E.(exec).
+
+(* empty program *)
+Definition exec_end
+  `{E : Exec PEnd (Tape 0 [] [])} := E.
+Compute exec_end.
+
+(* > *)
+Definition exec_right
+  `{E : Exec (PRight PEnd) (Tape 0 [] [])} := E.
 Check exec_right.
-Definition exec_nonterm `{E : Exec (PLoop PEnd PEnd) (Tape 1 [] [])} := E.(exec).
-Check exec_nonterm.
+
+(* >*>*[*<] *)
+Definition exec_rr_loop_l
+  `{E : Exec (PRight (PFlip (PRight (PFlip (PLoop (PFlip (PLeft PEnd)) PEnd)))))
+             (Tape 0 [] [])} := E.
+Check exec_rr_loop_l.
+
+(* [] *)
+Definition exec_loop_nonterm
+  `{E : Exec (PLoop PEnd PEnd) (Tape 1 [] [])} := E.
+Fail Timeout 1 Check exec_loop_nonterm.
+
+(* >*[>*] *)
+Definition exec_r_loop_r_nonterm
+  `{E : Exec (PRight (PFlip (PLoop (PRight (PFlip PEnd)) PEnd)))} := E.
+Fail Timeout 1 Check exec_r_loop_r_nonterm.
+
+(* "Hallo, Welt!"
+   >*>>>*>>>>>*>*>>>>>*>>*>*>>*>*>>>>*>*>>*>*>>>>*>*>>*>*>*>*>>>*>>*>*>>>
+   >>*>>>>>>>*>>*>>*>*>*>>*>*>>>*>>*>>*>*>>*>*>>>>*>*>*>>*>>>>>*>>>>>*> *)
+Definition exec_hworld
+  `{E : Exec (PRight (PFlip (PRight (PRight (PRight (PFlip (PRight (PRight
+             (PRight (PRight (PRight (PFlip (PRight (PFlip (PRight (PRight
+             (PRight (PRight (PRight (PFlip (PRight (PRight (PFlip (PRight
+             (PFlip (PRight (PRight (PFlip (PRight (PFlip (PRight (PRight
+             (PRight (PRight (PFlip (PRight (PFlip (PRight (PRight (PFlip
+             (PRight (PFlip (PRight (PRight (PRight (PRight (PFlip (PRight
+             (PFlip (PRight (PRight (PFlip (PRight (PFlip (PRight (PFlip
+             (PRight (PFlip (PRight (PRight (PRight (PFlip (PRight (PRight
+             (PFlip (PRight (PFlip (PRight (PRight (PRight (PRight (PRight
+             (PFlip (PRight (PRight (PRight (PRight (PRight (PRight (PRight
+             (PFlip (PRight (PRight (PFlip (PRight (PRight (PFlip (PRight
+             (PFlip (PRight (PFlip (PRight (PRight (PFlip (PRight (PFlip
+             (PRight (PRight (PRight (PFlip (PRight (PRight (PFlip (PRight
+             (PRight (PFlip (PRight (PFlip (PRight (PRight (PFlip (PRight
+             (PFlip (PRight (PRight (PRight (PRight (PFlip (PRight (PFlip
+             (PRight (PFlip (PRight (PRight (PFlip (PRight (PRight (PRight
+             (PRight (PRight (PFlip (PRight (PRight (PRight (PRight (PRight
+             (PFlip (PRight PEnd)))))))))))))))))))))))))))))))))))))))))))
+             ))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
+             ))))))))))))))))))))))))))))))))) (Tape 0 [] [])} := E.
+Check exec_hworld.
+Compute (rev exec_hworld.(exec).(ltape)).
